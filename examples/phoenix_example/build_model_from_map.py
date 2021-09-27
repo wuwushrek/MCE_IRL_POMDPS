e87ef6e30d325aa1020327660f814ad71d09cb04
wuwushrek <frmbouwe@gmail.com>
2021-09-27 13:44:11 -0500

    small fixes

--- examples/phoenix_example/build_model_from_map.py
+++ examples/phoenix_example/build_model_from_map.py
@@ -460,8 +460,12 @@ eps_bias = None
 n_row_focus = 40
 n_col_focus = 80
 focus_zone = (south_west_center[0], south_west_center[1], south_west_center[0]+n_row_focus, south_west_center[1]+n_col_focus)
-focus_init = (0, 0, n_row, n_col)
-# focus_init = (100-south_west_center[0], 110-south_west_center[1], 5, 5)
+# focus_init = (0, 0, n_row, n_col)
+focus_init_row = 100-south_west_center[0]
+focus_init_col = 100-south_west_center[1]
+focus_init_nrow = 5
+focus_init_ncol = 5
+focus_init = (focus_init_row, focus_init_col, focus_init_row+focus_init_nrow, focus_init_col+focus_init_ncol)
 
 # Build the feature distribution on the focused map
 final_map = build_map(traj_data, n_row_focus, n_col_focus, south_west_center=south_west_center, id_traj=id_traj, eps_bias=eps_bias)
@@ -469,155 +473,155 @@ final_map = build_map(traj_data, n_row_focus, n_col_focus, south_west_center=sou
 # Compute the transition matrix and the set of states and observation from the model
 m_obs_dict = None
 m_action_set = None
-# (trans_dict, state_set), (m_obs_dict, id_obs), (m_action_set, move) = build_pomdp(n_row_focus, n_col_focus, final_map, obs_radius=obs_radius)
+(trans_dict, state_set), (m_obs_dict, id_obs), (m_action_set, move) = build_pomdp(n_row_focus, n_col_focus, final_map, obs_radius=obs_radius)
 
 robot_obs_evol, robot_pos_evol = build_state_trajectories(m_obs_dict, m_robot_state_evol, id_traj, m_action_set, focus_zone)
 goal_set = [ m_traj[-1] for m_traj in robot_pos_evol] 
-# init_set = [ (i,j,featv) for (i, j, featv) in m_obs_dict.keys() if (i>=focus_init[0] and j>=focus_init[1] and i< focus_init[2] and j < focus_init[3])]
-init_set = []
+init_set = [ (i,j,featv) for (i, j, featv) in m_obs_dict.keys() if (i>=focus_init[0] and j>=focus_init[1] and i< focus_init[2] and j < focus_init[3])]
+# init_set = []
 # print(robot_obs_evol)
 # print(robot_pos_evol)
 
 # # Build the POMDP file
-# build_prism_model((final_map, trans_dict, state_set, m_obs_dict, id_obs), (n_row_focus, n_col_focus, focus_zone, obs_radius, id_traj, goal_set, init_set, robot_obs_evol, robot_pos_evol), m_action_set, 'test_phoenix')
+build_prism_model((final_map, trans_dict, state_set, m_obs_dict, id_obs), (n_row_focus, n_col_focus, focus_zone, obs_radius, id_traj, goal_set, init_set, robot_obs_evol, robot_pos_evol), m_action_set, 'test_phoenix')
 
 
-####################################################################################################
-############################################## Do SOME PLOTTING ####################################
+# ####################################################################################################
+# ############################################## Do SOME PLOTTING ####################################
 
-from matplotlib import pyplot as plt, cm
-import matplotlib.patches as patches
+# from matplotlib import pyplot as plt, cm
+# import matplotlib.patches as patches
 
-def arrowplot(axes, x, y, nArrs=30, mutateSize=10, color='gray', markerStyle='o'): 
-	'''arrowplot : plots arrows along a path on a set of axes
-		axes   :  the axes the path will be plotted on
-		x      :  list of x coordinates of points defining path
-		y      :  list of y coordinates of points defining path
-		nArrs  :  Number of arrows that will be drawn along the path
-		mutateSize :  Size parameter for arrows
-		color  :  color of the edge and face of the arrow head
-		markerStyle : Symbol
+# def arrowplot(axes, x, y, nArrs=30, mutateSize=10, color='gray', markerStyle='o'): 
+# 	'''arrowplot : plots arrows along a path on a set of axes
+# 		axes   :  the axes the path will be plotted on
+# 		x      :  list of x coordinates of points defining path
+# 		y      :  list of y coordinates of points defining path
+# 		nArrs  :  Number of arrows that will be drawn along the path
+# 		mutateSize :  Size parameter for arrows
+# 		color  :  color of the edge and face of the arrow head
+# 		markerStyle : Symbol
 	
-		Bugs: If a path is straight vertical, the matplotlab FanceArrowPatch bombs out.
-		  My kludge is to test for a vertical path, and perturb the second x value
-		  by 0.1 pixel. The original x & y arrays are not changed
+# 		Bugs: If a path is straight vertical, the matplotlab FanceArrowPatch bombs out.
+# 		  My kludge is to test for a vertical path, and perturb the second x value
+# 		  by 0.1 pixel. The original x & y arrays are not changed
 	
-		MHuster 2016, based on code by 
-	'''
-	# recast the data into numpy arrays
-	x = np.array(x, dtype='f')
-	y = np.array(y, dtype='f')
-	nPts = len(x)
+# 		MHuster 2016, based on code by 
+# 	'''
+# 	# recast the data into numpy arrays
+# 	x = np.array(x, dtype='f')
+# 	y = np.array(y, dtype='f')
+# 	nPts = len(x)
 
-	# Plot the points first to set up the display coordinates
-	# axes.plot(x,y, markerStyle, ms=2, color=color)
+# 	# Plot the points first to set up the display coordinates
+# 	# axes.plot(x,y, markerStyle, ms=2, color=color)
 
-	# get inverse coord transform
-	inv = axes.transData.inverted()
+# 	# get inverse coord transform
+# 	inv = axes.transData.inverted()
 
-	# transform x & y into display coordinates
-	# Variable with a 'D' at the end are in display coordinates
-	xyDisp = np.array(axes.transData.transform(list(zip(x,y))))
-	xD = xyDisp[:,0]
-	yD = xyDisp[:,1]
+# 	# transform x & y into display coordinates
+# 	# Variable with a 'D' at the end are in display coordinates
+# 	xyDisp = np.array(axes.transData.transform(list(zip(x,y))))
+# 	xD = xyDisp[:,0]
+# 	yD = xyDisp[:,1]
 
-	# drD is the distance spanned between pairs of points
-	# in display coordinates
-	dxD = xD[1:] - xD[:-1]
-	dyD = yD[1:] - yD[:-1]
-	drD = np.sqrt(dxD**2 + dyD**2)
+# 	# drD is the distance spanned between pairs of points
+# 	# in display coordinates
+# 	dxD = xD[1:] - xD[:-1]
+# 	dyD = yD[1:] - yD[:-1]
+# 	drD = np.sqrt(dxD**2 + dyD**2)
 
-	# Compensating for matplotlib bug
-	dxD[np.where(dxD==0.0)] = 0.1
+# 	# Compensating for matplotlib bug
+# 	dxD[np.where(dxD==0.0)] = 0.1
 
-	# rtotS is the total path length
-	rtotD = np.sum(drD)
+# 	# rtotS is the total path length
+# 	rtotD = np.sum(drD)
 
-	# based on nArrs, set the nominal arrow spacing
-	arrSpaceD = rtotD / nArrs
+# 	# based on nArrs, set the nominal arrow spacing
+# 	arrSpaceD = rtotD / nArrs
 
-	# Loop over the path segments
-	iSeg = 0
-	while iSeg < nPts - 1:
-		# Figure out how many arrows in this segment.
-		# Plot at least one.
-		nArrSeg = max(1, int(drD[iSeg] / arrSpaceD + 0.5))
-		xArr = (dxD[iSeg]) / nArrSeg # x size of each arrow
-		segSlope = dyD[iSeg] / dxD[iSeg]
-		# Get display coordinates of first arrow in segment
-		xBeg = xD[iSeg]
-		xEnd = xBeg + xArr
-		yBeg = yD[iSeg]
-		yEnd = yBeg + segSlope * xArr
-		# Now loop over the arrows in this segment
-		for iArr in range(nArrSeg):
-			# Transform the oints back to data coordinates
-			xyData = inv.transform(((xBeg, yBeg),(xEnd,yEnd)))
-			# Use a patch to draw the arrow
-			# I draw the arrows with an alpha of 0.5
-			p = patches.FancyArrowPatch( 
-				xyData[0], xyData[1], 
-				arrowstyle='simple',
-				mutation_scale=mutateSize,
-				color=color, alpha=0.5)
-			axes.add_patch(p)
-			# Increment to the next arrow
-			xBeg = xEnd
-			xEnd += xArr
-			yBeg = yEnd
-			yEnd += segSlope * xArr
-		# Increment segment number
-		iSeg += 1
-
-
-# Plot the obtained focused final map
-# Visualize the maps and the robots trajectories
-color_dist = {UNKNOWN_FEATURE : np.array([0, 0, 0]), 'grass' : np.array([11,102,35])/255.0,  'gravel' : np.array([1.0, 0, 0]), 'road' : np.array([0.5,0.5,0.5])}
-traj_spacing = 5
-mutateSize = 5
-nArrows = 1
-colorTrajectories = 'blue'
-initColor = 'cyan'
-endColor = 'yellow'
-
-# Rebuild the local image for showing the zone of interest
-m_local_map = np.zeros((n_row_focus, n_col_focus, 3))
-for i in range(n_row_focus):
-	for j in range(n_col_focus):
-		feat_dict = final_map[i,j]	
-		total_count = sum( v for key,v in feat_dict.items())
-		m_local_map[i,j] = np.zeros(3)
-		# print (i, j, feat_dict)
-		for elem, v in feat_dict.items():
-			m_local_map[i,j] += (float(v)/total_count) * color_dist[elem]
-
-# Create the figure and draw the image inside the figure
-plt.figure(figsize=(12,12))
-
-# Plot the resulting map and define correctly the axes
-info_color = plt.imshow(m_local_map, interpolation='none')
-# plt.gca().set_xticklabels([ int(val+south_west_center[1]) for val in plt.gca().get_xticks()])
-# plt.gca().set_yticklabels([ int(val+south_west_center[0]) for val in plt.gca().get_yticks()])
-
-# Plot the agent trajectories used to do the IRL problem
-for ind_traj_robot in robot_pos_evol:
-	x = np.array([ j for (i, j, fv) in ind_traj_robot])
-	y = np.array([ i for (i, j, fv) in ind_traj_robot])
-	arrowplot(plt.gca(), x[0:-1:traj_spacing], y[0:-1:traj_spacing], nArrs=nArrows, mutateSize=mutateSize, color=colorTrajectories, markerStyle='o')
-	# plt.plot(x[0:-1:20], y[0:-1:20])
-
-# Plot the initial and end zone
-plt.scatter([ j for (i,j,featv) in init_set], [ i for (i,j,featv) in init_set], color=initColor)
-plt.scatter([ j for (i,j,featv) in goal_set], [ i for (i,j,featv) in goal_set], color=endColor)
-# plt.plot([], y_delim, color='red', linewidth=2)
+# 	# Loop over the path segments
+# 	iSeg = 0
+# 	while iSeg < nPts - 1:
+# 		# Figure out how many arrows in this segment.
+# 		# Plot at least one.
+# 		nArrSeg = max(1, int(drD[iSeg] / arrSpaceD + 0.5))
+# 		xArr = (dxD[iSeg]) / nArrSeg # x size of each arrow
+# 		segSlope = dyD[iSeg] / dxD[iSeg]
+# 		# Get display coordinates of first arrow in segment
+# 		xBeg = xD[iSeg]
+# 		xEnd = xBeg + xArr
+# 		yBeg = yD[iSeg]
+# 		yEnd = yBeg + segSlope * xArr
+# 		# Now loop over the arrows in this segment
+# 		for iArr in range(nArrSeg):
+# 			# Transform the oints back to data coordinates
+# 			xyData = inv.transform(((xBeg, yBeg),(xEnd,yEnd)))
+# 			# Use a patch to draw the arrow
+# 			# I draw the arrows with an alpha of 0.5
+# 			p = patches.FancyArrowPatch( 
+# 				xyData[0], xyData[1], 
+# 				arrowstyle='simple',
+# 				mutation_scale=mutateSize,
+# 				color=color, alpha=0.5)
+# 			axes.add_patch(p)
+# 			# Increment to the next arrow
+# 			xBeg = xEnd
+# 			xEnd += xArr
+# 			yBeg = yEnd
+# 			yEnd += segSlope * xArr
+# 		# Increment segment number
+# 		iSeg += 1
 
 
-# print(plt.gca().get_xticks())
-# print(plt.gca().get_xticklabels())
-# plt.xticks(np.linspace(south_west_center[1], south_west_center[1]+n_col_focus, 11))
-# plt.yticks(np.linspace(south_west_center[0], south_west_center[0]+n_row_focus, 25))
-# plt.xlabel()
-# plt.colorbar(info_color)
-# print(info_color)
-plt.show()
-###################################################################################################
+# # Plot the obtained focused final map
+# # Visualize the maps and the robots trajectories
+# color_dist = {UNKNOWN_FEATURE : np.array([0, 0, 0]), 'grass' : np.array([11,102,35])/255.0,  'gravel' : np.array([1.0, 0, 0]), 'road' : np.array([0.5,0.5,0.5])}
+# traj_spacing = 5
+# mutateSize = 5
+# nArrows = 1
+# colorTrajectories = 'blue'
+# initColor = 'cyan'
+# endColor = 'yellow'
+
+# # Rebuild the local image for showing the zone of interest
+# m_local_map = np.zeros((n_row_focus, n_col_focus, 3))
+# for i in range(n_row_focus):
+# 	for j in range(n_col_focus):
+# 		feat_dict = final_map[i,j]	
+# 		total_count = sum( v for key,v in feat_dict.items())
+# 		m_local_map[i,j] = np.zeros(3)
+# 		# print (i, j, feat_dict)
+# 		for elem, v in feat_dict.items():
+# 			m_local_map[i,j] += (float(v)/total_count) * color_dist[elem]
+
+# # Create the figure and draw the image inside the figure
+# plt.figure(figsize=(12,12))
+
+# # Plot the resulting map and define correctly the axes
+# info_color = plt.imshow(m_local_map, interpolation='none')
+# # plt.gca().set_xticklabels([ int(val+south_west_center[1]) for val in plt.gca().get_xticks()])
+# # plt.gca().set_yticklabels([ int(val+south_west_center[0]) for val in plt.gca().get_yticks()])
+
+# # Plot the agent trajectories used to do the IRL problem
+# for ind_traj_robot in robot_pos_evol:
+# 	x = np.array([ j for (i, j, fv) in ind_traj_robot])
+# 	y = np.array([ i for (i, j, fv) in ind_traj_robot])
+# 	arrowplot(plt.gca(), x[0::traj_spacing+1], y[0::traj_spacing+1], nArrs=nArrows, mutateSize=mutateSize, color=colorTrajectories, markerStyle='o')
+# 	# plt.plot(x[0:-1:20], y[0:-1:20])
+
+# # Plot the initial and end zone
+# plt.scatter([ j for (i,j,featv) in init_set], [ i for (i,j,featv) in init_set], color=initColor)
+# plt.scatter([ j for (i,j,featv) in goal_set], [ i for (i,j,featv) in goal_set], color=endColor)
+# # plt.plot([], y_delim, color='red', linewidth=2)
+
+
+# # print(plt.gca().get_xticks())
+# # print(plt.gca().get_xticklabels())
+# # plt.xticks(np.linspace(south_west_center[1], south_west_center[1]+n_col_focus, 11))
+# # plt.yticks(np.linspace(south_west_center[0], south_west_center[0]+n_row_focus, 25))
+# # plt.xlabel()
+# # plt.colorbar(info_color)
+# # print(info_color)
+# plt.show()
+# ################################################################################################
